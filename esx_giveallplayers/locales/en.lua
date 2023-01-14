feef5b590e4e8f5b51063bbae98d3940afb36a55
0xShirooo <91967873+0xShirooo@users.noreply.github.com>
2023-01-14 01:30:03 +0100

    Add files via upload

---
+++ esx_giveallplayers/locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+	['subject'] = 'Server',
+	['subject2'] = 'Give notification',
+	['item_msg'] = 'All players got ~b~x%s ~y~%s',	
+	['weapon_msg'] = 'All players got ~b~x1 ~y~%s ~s~with ~g~%s ~s~ammo',
+	['money_msg'] = 'All players got cash ~g~$%s',
+	['bank_msg'] = 'All players got ~g~$%s ~s~into bank',
+	['black_msg'] = 'All players got black money ~r~$%s',
+	['unknown_item'] = 'unknown item',	
+	['unknown_weapon'] = 'unknown weapon',
+	['unknown_account'] = 'unknown account type:\n~y~%s',
+	['unknown_amount'] = 'you need type amount'
+}
